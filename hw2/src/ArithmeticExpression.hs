447ca032116761769a22f3687db35415a1268e3b
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-10-22 15:31:28 +0300

    [hw2] Add arithmetic expressions

---
+++ hw2/src/ArithmeticExpression.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE GADTs #-}
+-- {-# LANGUAGE StandaloneDeriving #-}
+
+module ArithmeticExpression
+       ( Expr (..)
+       , ArithmeticError
+       , eval
+       ) where
+
+import           Control.Applicative (liftA2)
+
+
+type ArithmeticError = String
+
+data Expr a
+  where
+    Const :: (Num a) => a -> Expr a
+    Sum   :: (Num a) => Expr a -> Expr a -> Expr a
+    Sub   :: (Num a) => Expr a -> Expr a -> Expr a
+    Mul   :: (Num a) => Expr a -> Expr a -> Expr a
+    Div   :: (RealFrac a) => Expr a -> Expr a -> Expr a
+    Pow   :: (Num a, Integral b) => Expr a -> Expr b -> Expr a
+
+-- deriving instance Show (Expr a)
+
+eval :: Expr a -> Either ArithmeticError a
+eval (Const a) = Right a
+eval (Sum x y) = liftA2 (+) (eval x) (eval y)
+eval (Sub x y) = liftA2 (-) (eval x) (eval y)
+eval (Mul x y) = liftA2 (*) (eval x) (eval y)
+eval (Pow x y) = liftA2 (^) (eval x) (eval y)
+eval (Div x y) = eval y >>= (\t -> if t == 0
+                 then Left "Division by zero"
+                 else eval x >>= (\s -> Right (s / t)))
