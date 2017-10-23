4ed5237c320f85e824ea13a153ddb17ae4f22f0e
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-10-23 03:28:44 +0300

    [hw2] Make combinations less deterministic

--- hw2/src/Nondeterministic.hs
+++ hw2/src/Nondeterministic.hs
@@ -7,16 +7,20 @@ module Nondeterministic
 
 bin :: Int -> [[Int]]
 bin 0 = [[]]
-bin n = [(0 :), (1 :)] <*> bin (n - 1)
+bin n = (:) <$> [0, 1] <*> bin (n - 1)
+-- bin = (flip replicate) [0, 1]
 
 combinations :: Int -> Int -> [[Int]]
-combinations _ 0 = [[]]
-combinations 0 _ = []
-combinations n k = combinations (n - 1) k ++ map (n :) (combinations (n - 1) (k - 1))
+combinations n = comb 1
+  where
+    comb from k
+        | k == 0    = [[]]
+        | otherwise = [from .. (n - k + 1)] >>= \first ->
+            (first :) <$> comb (first + 1) (k - 1)
 
 permutations :: [a] -> [[a]]
 permutations [] = [[]]
 permutations (l : ls) = permutations ls >>= interleave l
   where
     interleave x []       = [[x]]
-    interleave x (p : ps) = (x : p : ps) : map (p :) (interleave x ps)
+    interleave x (p : ps) = (x : p : ps) : ((p :) <$> interleave x ps)
