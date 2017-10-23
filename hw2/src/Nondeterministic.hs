243a5360053c07ded20a2f26cb20e9fb5f2935e3
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-10-23 10:51:38 +0300

    [hw2] Add sorted version of permutations

--- hw2/src/Nondeterministic.hs
+++ hw2/src/Nondeterministic.hs
@@ -2,13 +2,14 @@ module Nondeterministic
        ( bin
        , combinations
        , permutations
+       , permutations' -- sorted version
        ) where
 
 
 bin :: Int -> [[Int]]
 bin 0 = [[]]
 bin n = (:) <$> [0, 1] <*> bin (n - 1)
--- bin = (flip replicate) [0, 1]
+-- bin = (flip replicateM) [0, 1]
 
 combinations :: Int -> Int -> [[Int]]
 combinations n = comb 1
@@ -24,3 +25,12 @@ permutations (l : ls) = permutations ls >>= interleave l
   where
     interleave x []       = [[x]]
     interleave x (p : ps) = (x : p : ps) : ((p :) <$> interleave x ps)
+
+permutations' :: [a] -> [[a]]
+permutations' [] = [[]]
+permutations' (x : xs) = (flip splitAt xs <$> [0 .. (length xs)]) >>= \(lhs, rhs) ->
+    permutations' lhs >>= \l ->
+    permutations' rhs >>= \r ->
+    return (l ++ [x] ++ r)
+--    return ([l, return x, r] >>= id)
+--    (++) <$> ((++) <$> permutations' lhs <*> [[x]]) <*> permutations' rhs -- TODO: get rid of (++)
