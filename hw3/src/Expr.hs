84d0f29ebd1d9bbcab4c0ab3d47b10beda13f944
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-11-26 14:15:57 +0300

    [hw3] Rewrite eval in monadic style - ignore errors by now

--- hw3/src/Expr.hs
+++ hw3/src/Expr.hs
@@ -1,31 +1,56 @@
+-- {-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Expr
        ( Expr (..)
        , eval
+       , Eval
+       , runEval
        ) where
 
-import qualified Data.Map as Map
+import           Control.Monad.Identity
+import qualified Data.Map               as Map
 
+type Name = String
 
 data Expr = Lit Int
-          | Var String
+          | Var Name
           | Add Expr Expr
           | Mul Expr Expr
           | Div Expr Expr
-          | Let String Expr Expr
+          | Let Name Expr Expr
     deriving (Show)
 
-eval :: Expr -> Map.Map String Int -> Either String Int
-eval (Lit x) _ = Right x
+{-
+newtype Eval a = Eval { runEval :: a }
+    deriving (Functor, Applicative, Monad)
+-- -}
+
+-- {-
+type Eval = Identity
+runEval :: Eval a -> a
+runEval = runIdentity
+-- -}
+
+eval :: Expr -> Map.Map Name Int -> Eval Int
+eval (Lit x) _ = return x
 eval (Var x) m = case Map.lookup x m of
-    Just v  -> Right v
-    Nothing -> Left ("No value for Var " ++ x ++ "; map = " ++ show m)
-eval (Add l r) m = eval l m >>= \lhs ->
-    eval r m >>= \rhs -> Right (lhs + rhs)
-eval (Mul l r) m = eval l m >>= \lhs ->
-    eval r m >>= \rhs -> Right (lhs * rhs)
-eval (Div l r) m = eval l m >>= \lhs ->
-    eval r m >>= \rhs -> if rhs == 0
-    then Left "Division by zero"
-    else Right (lhs * rhs)
-eval (Let v a e) m = eval a m >>= \newval ->
+    Just v  -> return v
+    Nothing -> fail $ "Var " ++ x ++ " is not bounded"
+eval (Add l r) m = do
+    lhs <- eval l m
+    rhs <- eval r m
+    return $ lhs + rhs
+eval (Mul l r) m = do
+    lhs <- eval l m
+    rhs <- eval r m
+    return $ lhs * rhs
+eval (Div l r) m = do
+    lhs <- eval l m
+    rhs <- eval r m
+    if rhs == 0
+    then fail "Division by zero"
+    else return $ lhs * rhs
+eval (Let v a e) m = do
+    newval <- eval a m
     eval e (Map.insert v newval m)
