2133c55616a71f8cded0e211856d4c5143f0363f
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-12-22 12:26:58 +0300

    [hw4] Add hw4 module and implement chooseByIndices

---
+++ hw4/README.md
@@ -0,0 +1,3 @@
+# hw4
+
+[![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/vadimsemenov/fuphow/blob/master/hw4/LICENSE)
---
+++ hw4/src/TemplateLib.hs
@@ -0,0 +1,35 @@
+-- {-# LANGUAGE TemplateHaskell #-}
+
+module TemplateLib
+       ( chooseByIndices
+       ) where
+
+
+import           Language.Haskell.TH
+
+import           Data.List           (sort)
+import qualified Data.Map            as Map
+
+
+chooseByIndices :: Int -> [Int] -> Q Exp
+chooseByIndices len indices = do
+    let unique :: [Int]
+        unique = nub indices
+    names <- mapM (newName . ('i' :) . show) unique
+    let mapping :: Map.Map Int Name
+        mapping = Map.fromList (zip unique names)
+    let list :: [PatQ]
+        list = [maybe wildP varP x | x <- map (mapping Map.!?) [0 .. len - 1]]
+    let result :: [ExpQ]
+        result = map (varE . (mapping Map.!)) indices
+    lamE [tupP list] (tupE result)
+
+
+nub :: Ord a => [a] -> [a]
+nub = nub' . sort
+  where
+    nub' [] = []
+    nub' [a] = [a]
+    nub' (a : b : rest) = if a == b
+                          then nub (b : rest)
+                          else a : nub (b : rest)
