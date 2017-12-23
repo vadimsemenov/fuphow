84533b268c75a636263e415f96be200567c8b4d0
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-12-24 00:42:52 +0300

    [hw4] Fix Renew and add proofs

--- hw4/src/Comonads/Renew.hs
+++ hw4/src/Comonads/Renew.hs
@@ -6,6 +6,7 @@ module Comonads.Renew
        ) where
 
 import           Control.Comonad (Comonad (..))
+import           Data.Monoid     ((<>))
 
 
 data Renew s e a = Renew (e -> a) s
@@ -18,5 +19,30 @@ instance Functor (Renew s e) where
 
 instance MonoidAction s e => Comonad (Renew s e) where
     extract   (Renew f _) = f mempty
-    extend g  (Renew f s) = Renew (g . Renew f . act s) s
-    duplicate (Renew f s) = Renew (Renew f . act s) s
+    extend g  (Renew f s) = Renew (g . (\e -> Renew (\e' -> f (e' <> e)) (act s e))) s
+    duplicate (Renew f s) = Renew (\e -> Renew (\e' -> f (e' <> e)) (act s e)) s
+
+{-
+extract . duplicate = id
+extract (duplicate (Renew f s)) =
+= extract $ Renew (\e -> Renew (\e' -> f (e' <> e)) (act s e)) s
+= (\e -> Renew (\e' -> f (e' <> e)) (act s e)) mempty
+= Renew (\e' -> f (e' <> mempty)) (act s mempty)
+= Renew (\e' -> f e') s
+= Renew f s
+
+fmap extract . duplicate = id
+fmap extract (duplicate (Renew f s))
+= fmap extract $ Renew (\e -> Renew (\e' -> f (e' <> e)) (act s e)) s
+= Renew (\e -> extract (Renew (\e' -> f (e' <> e)) (act s e))) s
+= Renew (\e -> (\e' -> f (e' <> e)) mempty) s
+= Renew (\e -> f (mempty <> e)) s
+= Renew (\e -> f e) s
+= Renew f s
+
+duplicate . duplicate = fmap duplicate . duplicate
+fmap duplicate (duplicate (Renew f s))
+= fmap duplicate (Renew (\e -> Renew (\e' -> f (e' <> e)) (act s e)) s)
+= Renew (\e -> duplicate (Renew (\e' -> f (e' <> e)) (act s e))) s
+= TODO
+-- -}
