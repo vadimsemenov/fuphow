793828cc8d0e342686dab2f5cfb4124e24c69974
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-12-26 21:32:32 +0300

    [hw4] Add extended functions for lenses

--- hw4/src/Lenses/Basic.hs
+++ hw4/src/Lenses/Basic.hs
@@ -6,8 +6,15 @@ module Lenses.Basic
        , set
        , view
        , over
+       , (.~)
+       , (^.)
+       , (%~)
        , _1
        , _2
+       , lens
+       , choosing
+       , (<%~)
+       , (<<%~)
        ) where
 
 import           Data.Functor.Const    (Const (..))
@@ -36,3 +43,30 @@ _1 f (a, x) = (\y -> (y, x)) <$> f a
 -- _2 :: Functor f => (a -> f b) -> (x, a) -> f (x, b)
 _2 :: Lens (x, a) (x, b) a b
 _2 f (x, a) = (\y -> (x, y)) <$> f a
+
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens getter setter f s = setter s <$> f (getter s)
+
+(.~) :: Lens s t a b -> b -> s -> t
+l .~ b = runIdentity . l (const $ Identity b)
+(^.) :: s -> Lens s t a b -> a
+s ^. l = getConst $ l Const s
+(%~) :: Lens s t a b -> (a -> b) -> s -> t
+l %~ f = runIdentity . l (Identity . f)
+
+-- Объединить две линзы в одну, которая работает с Either.
+choosing :: Lens s1 t1 a b
+         -> Lens s2 t2 a b
+         -> Lens (Either s1 s2) (Either t1 t2) a b
+choosing l1 l2 = lens (either (^.l1) (^.l2))
+                      (either (\s1 b -> Left  $ (l1.~b) s1)
+                              (\s2 b -> Right $ (l2.~b) s2))
+
+-- Изменить цель линзы и вернуть новый результат. Постарайтесь
+-- реализовать без анонимных функций и определений своих функций
+(<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
+(<%~) l f s = (f (s ^. l), (l %~ f) s)
+
+-- Изменить цель линзы, но вернуть старый результат.
+(<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
+(<<%~) l f s = (s ^. l, (l %~ f) s)
