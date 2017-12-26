93cd3f74f428559bac79d5cdfd85818f07bb5e73
Vadim Semenov <vadim.pillarbox@gmail.com>
2017-12-26 23:34:27 +0300

    [hw4] Introduce File System ADT

---
+++ hw4/src/Lenses/FileSystem.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE LambdaCase      #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Lenses.FileSystem
+       ( FS (..)
+       , getFS
+       ) where
+
+import           Control.Lens
+import           System.Directory (doesDirectoryExist, doesFileExist, listDirectory)
+import           System.FilePath  (splitPath, (</>))
+
+data FS
+    = Dir
+          { _name     :: FilePath  -- название папки, не полный путь
+          , _contents :: [FS]
+          }
+    | File
+          { _name     :: FilePath  -- название файла, не полный путь
+          }
+
+instance Show FS where
+    show = show' 0
+      where
+        show' offset (File n)   = spaces' offset ++ n
+        show' offset (Dir n cs) = let cur = spaces' offset ++ n in
+            cur ++ concatMap (("\n" ++) . show' (length cur)) cs
+        spaces' n = replicate n ' '
+
+makeLenses ''FS
+makePrisms ''FS
+
+getFS :: FilePath -> IO FS
+getFS path = (,) <$> doesFileExist path <*> doesDirectoryExist path >>= \case
+    (True, False) -> return $ File path
+    (False, True) -> Dir (last $ splitPath path) <$>
+        (listDirectory path >>= mapM (getFS . (path </>)))
+    _             -> error $ "Error on path: " ++ path
+
+{-
+λ> getFS "/Users/vadim/Documents/fuphow/hw4/src"
+src
+   Comonads
+           /Users/vadim/Documents/fuphow/hw4/src/Comonads/ProjectBuilder.hs
+           /Users/vadim/Documents/fuphow/hw4/src/Comonads/Renew.hs
+           /Users/vadim/Documents/fuphow/hw4/src/Comonads/RoseTree.hs
+   Lenses
+         /Users/vadim/Documents/fuphow/hw4/src/Lenses/Basic.hs
+         /Users/vadim/Documents/fuphow/hw4/src/Lenses/FileSystem.hs
+   /Users/vadim/Documents/fuphow/hw4/src/TemplateLib.hs
+it :: FS
+-- -}
